8f17844e8f487ac00d03d485ae1c3a72a9bf555a
Your Name <agunggema@gmail.com>
2026-06-17 20:03:02 +0700

    update vite.config.ts and package.json to support node 18 and above

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      tailwindcss: path.resolve(
+        __dirname,
+        "node_modules/tailwindcss/dist/lib.mjs"
+      ),
+    },
+  },
   build: {
     rollupOptions: {
       output: {
