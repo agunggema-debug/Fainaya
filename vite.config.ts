db2d7dd7d9ed26baae58b5daed4a108096d2214a
Your Name <agunggema@gmail.com>
2026-06-17 17:40:56 +0700

    update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
+      external: ["tailwindcss"],
       output: {
         manualChunks(id) {
           // Memisahkan Supabase ke dalam chunk tersendiri
-          if (id.includes('@supabase')) {
-            return 'supabase';
+          if (id.includes("@supabase")) {
+            return "supabase";
           }
           // Memisahkan dependencies inti lainnya
-          if (id.includes('node_modules')) {
-            return 'vendor';
+          if (id.includes("node_modules")) {
+            return "vendor";
           }
         },
       },
     },
   },
-})
+});
